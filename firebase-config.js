5551a33167dc5af4e3424b72131cfd11989ae319
ashx <ashx@pop-os.localdomain>
2022-10-08 16:25:49 +0530

    add login, register page firebase config, tailwind

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyBcnRFxyChEbM26DmeANxTxARJlXSB7Gwo",
+  authDomain: "my-app-fffe8.firebaseapp.com",
+  projectId: "my-app-fffe8",
+  storageBucket: "my-app-fffe8.appspot.com",
+  messagingSenderId: "778308273023",
+  appId: "1:778308273023:web:bef809a6c6a32c5b96d04a",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
